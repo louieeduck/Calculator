d21792af55121e0ec85510dc73f1ed5e5f2e4408
Evan <evanfretheim@gmail.com>
2021-07-16 12:25:56 -0500

    comments

--- JS/calculator.js
+++ JS/calculator.js
@@ -1,7 +1,7 @@
 const Calculator = {
     Display_Value: '0',
     First_Operand: null,
-    Wait_Second_Operand: false,
+    Wait_Second_Operand: false,         // Start calculator off with 0 displayed
     operator: null,
 };
 
@@ -11,19 +11,19 @@ function Input_Digit(digit) {
         Calculator.Display_Value = digit;
         Calculator.Wait_Second_Operand = false;
     } else {
-        Calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit;
+        Calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit;           // Creating the displayed digits 
     }
 }
 
 function Input_Decimal(dot) {
-    if(Calculator_Reset.Wait_Second_Operand === true) return;
+    if(Calculator_Reset.Wait_Second_Operand === true) return;           // Create the deciaml point function
     if(!Calculator.Display_Value.includes(dot)) {
         Calculator.Display_Value += dot;
     }
 }
 
 function Handle_Operator(Next_Operator) {
-    const { First_Operand, Display_Value, operator } = Calculator
+    const { First_Operand, Display_Value, operator } = Calculator       // 
     const Value_of_Input = parseFloat(Display_Value);
     if (operator && Calculator.Wait_Second_Operand) {
         Calculator.operator = Next_Operator;
@@ -33,7 +33,7 @@ function Handle_Operator(Next_Operator) {
         Calculator.First_Operand = Value_of_Input;
     } else if (operator) {
         const Value_Now = First_Operand || 0;
-        let result = Perform_Calculation[operator] (Value_Now, Value_of_Input);
+        let result = Perform_Calculation[operator] (Value_Now, Value_of_Input);         // 
         result = Number(result) .toFixed(9)
         result = (result * 1).toString()
         Calculator.Display_Value = parseFloat(result);
@@ -48,19 +48,19 @@ const Perform_Calculation = {
     '/': (First_Operand, Second_Operand) => First_Operand / Second_Operand,
     '*': (First_Operand, Second_Operand) => First_Operand * Second_Operand,
     '+': (First_Operand, Second_Operand) => First_Operand + Second_Operand,
-    '-': (First_Operand, Second_Operand) => First_Operand - Second_Operand,
+    '-': (First_Operand, Second_Operand) => First_Operand - Second_Operand,         // Creates the operator functions
     '=': (First_Operand, Second_Operand) => Second_Operand
 };
 
 function Calculator_Reset() {
     Calculator.Display_Value = '0';
-    Calculator.First_Operand = null;
-    Calculator.Wait_Second_Operand = false;
+    Calculator.First_Operand = null;    
+    Calculator.Wait_Second_Operand = false;                             // Set back to cleared, 0 displayed
     Calculator.operator = null;
 }
 
 function Update_Display() {
-    const display = document.querySelector('.calculator-screen');
+    const display = document.querySelector('.calculator-screen');       // refresh the screen to show the selected keys
     display.value = Calculator.Display_Value;
 }
 
@@ -81,7 +81,7 @@ keys.addEventListener('click', (event) => {
         Update_Display();
         return;
     }
-    if (target.classList.contains('all-clear')) {
+    if (target.classList.contains('all-clear')) {                   // linking JS to html created keys
         Calculator_Reset();
         Update_Display();
         return;
